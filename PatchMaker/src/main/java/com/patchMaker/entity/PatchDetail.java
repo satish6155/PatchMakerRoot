5dae4d0161b9633b1899a9e53e6c4aad6267ba49
deepalijain48 <63190360+deepalijain48@users.noreply.github.com>
2020-04-05 17:30:51 +0530

    User Authentication code merged.
    Patch Maker Authentication work done.

---
+++ PatchMaker/src/main/java/com/patchMaker/entity/PatchDetail.java
@@ -0,0 +1,17 @@
+package com.patchMaker.entity;
+
+public class PatchDetail {
+
+	
+	String name;
+	String neosfr;
+	@Override
+	
+	
+	public String toString() {
+		return "PatchDetail [name=" + name + ", neosfr=" + neosfr + "]";
+	}
+	
+	
+	
+}
